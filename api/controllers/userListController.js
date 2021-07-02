a9390ba0b255ce5585bef3fc171dca6e9dbfc64d
Sophon-Kr <77550438+Sophon-Kr@users.noreply.github.com>
2021-07-02 17:18:47 +0700

    edit userModel.js

--- api/controllers/userListController.js
+++ api/controllers/userListController.js
@@ -29,12 +29,8 @@ const getUserById = (request, response) => {
 }
 
 const createUser = (request, response) => {
-//   const { name, email } = request.body
-    const user = new user({
-        name: request.body.name,
-        email: request.body.email
-       
-    });
+  const { name, email } = request.body
+  
 
   pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [user.name, user.email], (error, results) => {
     if (error) {
---
+++ api/models/userModel.js
@@ -0,0 +1,5 @@
+const user = new User({
+    name: request.body.name,
+    email: request.body.email
+   
+});
